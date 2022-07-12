58948f109f022b65782c50a6a0a2d0b3d7819b84
matheus <mattmvc56@gmail.com>
2022-07-11 20:44:21 -0400

    Initial Commit

---
+++ httprequest/httprequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  httprequestApp.swift
+//  httprequest
+//
+//  Created by Matheus on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct httprequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
