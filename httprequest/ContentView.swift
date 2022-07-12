48580559f91402ac8f12d1831740f2d53d1a7e35
matheus <mattmvc56@gmail.com>
2022-07-11 22:22:16 -0400

    "festa: inicial componentes"

--- httprequest/ContentView.swift
+++ httprequest/ContentView.swift
@@ -7,10 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct URLImage: View {
+    let urlString: String
+    
+    @State var data: Data?
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+        } else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    private func fetchData(){
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.courses, id: \.self){ course in
+                    HStack {
+                        URLImage(urlString: course.image)
+
+                        Text(course.name).bold()
+                    }
+                    .padding(3)
+                    
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
     }
 }
 
