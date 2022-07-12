65a5ed06739d95eca340f49498197e3993f855b1
matheus <mattmvc56@gmail.com>
2022-07-11 22:27:13 -0400

    first

--- httprequest/ContentView.swift
+++ httprequest/ContentView.swift
@@ -68,3 +68,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Previews_ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
